ea13a8f50cca16c283bb3243328f6c3f9fb1bcc3
XcantloadX <3188996979@qq.com>
2026-03-13 20:30:44 +0800

    fix(task): 修复启动游戏时有几率卡在返回首页
    原因是有时候处于活动期间，LIVE 按钮的图标会在三个图标之间轮播。而原来有判断 LIVE 图标稳定的逻辑，现在由于图标轮播导致稳定逻辑一直无法满足稳定条件，于是卡住。

--- iaa/tasks/common.py
+++ iaa/tasks/common.py
@@ -21,13 +21,8 @@ def go_home(threshold_timeout: float = 0):
     cd = Countdown(threshold_timeout)
     for _ in Loop():
         if R.Hud.ButtonLive.find():
-            cd.start()
-            logger.debug('Crystal icon found.')
-            # 因为进入游戏后，公告弹窗会延迟弹出，因此不可以立即返回
-            # 必须等待一段时间，关掉公告后再返回
-            if cd.expired():
-                logger.info('Now at home.')
-                break
+            logger.debug('Live button found.')
+            break
         elif R.Hud.ButtonGoBack.try_click():
             logger.debug('Go back button found and clicked.')
         else:
